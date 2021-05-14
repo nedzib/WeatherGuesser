f0e4bff84f038e22a4be9c80a26671a19fc3559d
Nedzib <nedzib.sastoque@gmail.com>
2021-05-13 21:16:21 -0500

    layouts in docs and some modules created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {
+      useHash: true,
+      preloadingStrategy: PreloadAllModules
+    }
+    )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

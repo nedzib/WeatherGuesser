bb5912fb99bad1d6d51b43203717e41f3ef5b700
Nedzib <nedzib.sastoque@gmail.com>
2021-06-28 10:28:19 -0500

    added some components and assets and also layouts

--- src/app/modules/home/home/home.component.ts
+++ src/app/modules/home/home/home.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public temp: boolean = true;
+  public name: string = '';
+  public payload = {
+    temp: true,
+    name: 'Player'
+  };
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log('--Home--');
   }
 
+  public onSubmit(action: string): void{
+    if (action === 'start'){
+      console.log('--onSubmit--Start');
+      console.log(this.payload);
+    } else if (action === 'about'){
+      console.log('--onSubmit--About');
+    }
+  }
+
+  public onChange(state: boolean): void{
+    // console.log('--onChange--');
+    this.payload.temp = state;
+  }
+
+  public onChangeName(data: any): void{
+    // console.log(data.srcElement.value);
+    this.payload.name = data.srcElement.value;
+  }
 }

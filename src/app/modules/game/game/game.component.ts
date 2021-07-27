74d2db26841baf39c063374fdc479c06c28bfa8d
Nedzib <nedzib.sastoque@gmail.com>
2021-07-27 08:13:14 -0500

    api from env fixed

--- src/app/modules/game/game/game.component.ts
+++ src/app/modules/game/game/game.component.ts
@@ -2,14 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import { CapitalService } from 'src/app/services/capital.service';
 import { ClimaService } from 'src/app/services/clima.service';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+  styleUrls: ['./game.component.css'],
 })
 export class GameComponent implements OnInit {
-
   public score = 0;
   public country = '';
   public capital = '';
@@ -17,17 +16,16 @@ export class GameComponent implements OnInit {
   public payload = {
     temp: true,
     name: 'Player',
-    score: 0
+    score: 0,
   };
 
   public rightOpt = 4;
 
-  public opt = [0,0,0]
+  public opt = [0, 0, 0];
 
   // NGClass Variables
   public right = [false, false, false];
   public wrong = [false, false, false];
-
 
   constructor(
     private UService: UsuarioService,
@@ -41,9 +39,9 @@ export class GameComponent implements OnInit {
     let infoCountry = {
       capital: '',
       country: '',
-      capitalID: ''
+      capitalID: '',
     };
-    while(infoCountry.capital === '' || infoCountry.country === ''){
+    while (infoCountry.capital === '' || infoCountry.country === '') {
       infoCountry = await this.CService.randomCapital();
     }
     console.log(infoCountry);
@@ -53,72 +51,69 @@ export class GameComponent implements OnInit {
     this.capital = infoCountry.capital;
     this.payload = this.UService.getSessionData();
     console.log(this.payload);
-    if(this.payload.temp === true){
+    if (this.payload.temp === true) {
       const infoWeatherC = await this.WService.weatherC(infoCountry.capitalID);
       this.fillData(infoWeatherC);
     } else {
       const infoWeatherF = await this.WService.weatherF(infoCountry.capitalID);
       this.fillData(infoWeatherF);
     }
-    
+  }
 
-  }  
-
-  public onSelect(value: any): void{
+  public onSelect(value: any): void {
     let audio = new Audio();
-    if(value === this.rightOpt){
+    if (value === this.rightOpt) {
       console.log('right');
       this.right[value] = true;
       this.score = this.score + 1;
-      this.payload.score = this.score
-      audio.src = "assets/sounds/right.mp3";
+      this.payload.score = this.score;
+      audio.src = 'assets/sounds/right.mp3';
       audio.play();
       this.reload();
     } else {
       console.log('NO right');
       this.wrong[value] = true;
       this.score = this.score - 1;
-      this.payload.score = this.score
-      audio.src = "assets/sounds/noright.mp3";
+      this.payload.score = this.score;
+      audio.src = 'assets/sounds/noright.mp3';
       audio.play();
     }
   }
 
-
-  private fillData(temps: any): void{
+  private fillData(temps: any): void {
     console.log(temps);
-    const rightOpt = (Math.floor(Math.random() * 3));
+    const rightOpt = Math.floor(Math.random() * 3);
     this.rightOpt = rightOpt;
-    this.opt[rightOpt] = temps.temp
-    if(rightOpt === 0){
+    this.opt[rightOpt] = temps.temp;
+    if (rightOpt === 0) {
       this.opt[1] = temps.opt1;
       this.opt[2] = temps.opt2;
     }
-    if(rightOpt === 1){
+    if (rightOpt === 1) {
       this.opt[0] = temps.opt1;
       this.opt[2] = temps.opt2;
     }
-    if(rightOpt === 2){
+    if (rightOpt === 2) {
       this.opt[1] = temps.opt1;
       this.opt[0] = temps.opt2;
     }
   }
 
-  private async reload(): Promise<void>{
+  private async reload(): Promise<void> {
     this.resetButtons();
     let infoCountry = {
       capital: '',
       country: '',
-      capitalID: ''
+      capitalID: '',
     };
-    while(infoCountry.capital === '' || infoCountry.country === ''){
+    while (infoCountry.capital === '' || infoCountry.country === '') {
       infoCountry = await this.CService.randomCapital();
     }
     console.log(infoCountry);
     this.country = infoCountry.country;
     this.capital = infoCountry.capital;
     console.log(this.payload);
-    if(this.payload.temp === true){
+    if (this.payload.temp === true) {
       const infoWeatherC = await this.WService.weatherC(infoCountry.capitalID);
       this.fillData(infoWeatherC);
     } else {
@@ -127,22 +122,14 @@ export class GameComponent implements OnInit {
     }
   }
 
-  private resetButtons(): void{
+  private resetButtons(): void {
     this.right = [false, false, false];
     this.wrong = [false, false, false];
   }
 
-  private wait(ms: number){
-    var start = new Date().getTime();
-    var end = start;
-    while(end < start + ms) {
-      end = new Date().getTime();
-    }
-  }
-
-  public onSubmit(): void{
+  public onSubmit(): void {
     console.log('--onSubmit--End');
-      this.UService.setSessionData(this.payload);
-      this.router.navigate(['/end']);
+    this.UService.setSessionData(this.payload);
+    this.router.navigate(['/end']);
   }
 }
--- src/app/services/clima.service.ts
+++ src/app/services/clima.service.ts
@@ -1,53 +1,57 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ClimaService {
-
-
   temp = 0;
 
-  constructor() { }
+  constructor() {}
 
-  private randomNumber(): number{
+  private randomNumber(): number {
     return Math.floor(Math.random() * 10) + 1;
   }
 
   public async weatherC(capitol: string): Promise<any> {
-    await fetch('https://api.openweathermap.org/data/2.5/weather?q='+capitol+'&appid=70045178fbb0b907f253483b592728e5')
-    .then(response => response.json())
-    .then(data => {
-      try {
-        this.temp = Math.floor(data.main.temp - 273.15);
-        return {
-          temp: this.temp,
-          opt1: this.temp+this.randomNumber(),
-          opt2: this.temp+this.randomNumber()
-        };
-      } catch (error) {
-        console.log(error);
-       return 'error'; 
-      }
-    });
-            return {
-          temp: this.temp,
-          opt1: this.temp+this.randomNumber(),
-          opt2: this.temp+this.randomNumber()
-        };
-  }
-
-  public async weatherF(capitol: string): Promise<any> {
-    await fetch('https://api.openweathermap.org/data/2.5/weather?q='+capitol+'&appid=70045178fbb0b907f253483b592728e5')
-    .then(response => response.json())
-    .then(data => {
-      this.temp = Math.floor((data.main.temp-273.15)*(9/5)+32);
-    });
+    await fetch(
+      'https://api.openweathermap.org/data/2.5/weather?q=' +
+        capitol + environment.apiOpenWeather
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        try {
+          this.temp = Math.floor(data.main.temp - 273.15);
+          return {
+            temp: this.temp,
+            opt1: this.temp + this.randomNumber(),
+            opt2: this.temp + this.randomNumber(),
+          };
+        } catch (error) {
+          console.log(error);
+          return 'error';
+        }
+      });
     return {
       temp: this.temp,
-      opt1: this.temp+this.randomNumber(),
-      opt2: this.temp+this.randomNumber()
+      opt1: this.temp + this.randomNumber(),
+      opt2: this.temp + this.randomNumber(),
     };
   }
 
+  public async weatherF(capitol: string): Promise<any> {
+    await fetch(
+      'https://api.openweathermap.org/data/2.5/weather?q=' +
+        capitol + environment.apiOpenWeather
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        this.temp = Math.floor((data.main.temp - 273.15) * (9 / 5) + 32);
+      });
+    return {
+      temp: this.temp,
+      opt1: this.temp + this.randomNumber(),
+      opt2: this.temp + this.randomNumber(),
+    };
+  }
 }
